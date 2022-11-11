4055e5f1a87a3babdb668efd3cb5a4ef4e5f860d
Yeras1k <yerasylhello@gmail.com>
2022-11-12 04:22:39 +0600

    erfgfff

--- main.py
+++ main.py
@@ -17,9 +17,9 @@ curator_password = "SeniorsTop"
 
 @bot.message_handler(commands=["start"])
 def first(message):
-    keyboard = types.InLineKeyboardMarkup()
-    student = types.InLineKeyboardMarkup(text = "Ученик", callback_data = 'student')
-    curator = types.InLineKeyboardMarkup(text = "Куратор", callback_data = 'curator')
+    keyboard = telebot.types.InlineKeyboardMarkup()
+    student = telebot.types.InlineKeyboardMarkup(text = "Ученик", callback_data = 'student')
+    curator = telebot.types.InlineKeyboardMarkup(text = "Куратор", callback_data = 'curator')
     keyboard.add(student, curator)
     send = bot.send_message(message.chat.id, f"Hello, {message.from_user.first_name}! Выберите роль", reply_markup=keyboard)
     bot.register_next_step_handler(send, second)
