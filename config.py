a19b93af92cf61b867edd4f740c23c77b458a830
Yeras1k <yerasylhello@gmail.com>
2022-11-11 21:27:03 +0600

    new table

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 BOT_TOKEN = "5786567515:AAHsu9ATx0upW7cRTAFbpi3WU2OhKjB-6Eg"
 APP_URL = "https://telebot-production-dce6.up.railway.app/" + BOT_TOKEN
-DB_URI = "postgresql://postgres:YyjpgZmHSE85Aa8dtlsF@containers-us-west-117.railway.app:5834/railway"
+DB_URI = "postgresql://postgres:TxRyqIQl7rWUpOniMcjx@containers-us-west-117.railway.app:7431/railway"
