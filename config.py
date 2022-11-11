57e0562f3e29d8d94fdc25a5dcd4cc4104895ad8
Yeras1k <yerasylhello@gmail.com>
2022-11-11 18:37:46 +0600

    database1

--- config.py
+++ config.py
@@ -1,2 +1,3 @@
 BOT_TOKEN = "5786567515:AAHsu9ATx0upW7cRTAFbpi3WU2OhKjB-6Eg"
 APP_URL = "https://telebot-production-dce6.up.railway.app/" + BOT_TOKEN
+DB_URI = "postgresql://postgres:yHY0E49dKhej5Zp1J4fG@containers-us-west-117.railway.app:5555/railway"
--- main.py
+++ main.py
@@ -9,7 +9,8 @@ server = Flask(__name__)
 logger = telebot.logger
 logger.setLevel(logging.DEBUG)
 
-
+db_connection = psycopg2.connect(DB_URI, sslmode="require")
+db_object = db_connection.cursor()
 
 @bot.message_handler(commands=["start"])
 def start(message):
