ef893cf22e48ee3b3a30b254f9a5b11933f2aac3
Yeras1k <yerasylhello@gmail.com>
2022-11-12 04:19:35 +0600

    ffffffff

--- main.py
+++ main.py
@@ -13,7 +13,6 @@ logger.setLevel(logging.DEBUG)
 
 db_connection = psycopg2.connect(DB_URI, sslmode="require")
 db_object = db_connection.cursor()
-
 curator_password = "SeniorsTop"
 
 @bot.message_handler(commands=["start"])
