e0bb32928028f791341235f7f8efdf08a3113975
Yeras1k <yerasylhello@gmail.com>
2022-11-12 07:16:29 +0600

    edvdvd

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 BOT_TOKEN = "5786567515:AAHsu9ATx0upW7cRTAFbpi3WU2OhKjB-6Eg"
 APP_URL = "https://telebot-production-dce6.up.railway.app/" + BOT_TOKEN
-DB_URI = "postgresql://postgres:MW54vHvb3EhfRIphPVry@containers-us-west-118.railway.app:7042/railway"
+DB_URI = "mysql://root:CFH6kF0jKWGJoiRuWKtc@containers-us-west-112.railway.app:8004/railway"
